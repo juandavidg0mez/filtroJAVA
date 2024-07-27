28d8d1cc5ce4b441c2d9b2d7712232ddfafaff94
juandavidg0mez <nihlgomez@gamil.com>
2024-07-26 20:39:11 -0500

    En serio necesito pasar por mismo edite el archivo me puse a leerlo completo

--- juegojava/src/main/java/com/javaplay/DatabaseConfig.java
+++ juegojava/src/main/java/com/javaplay/DatabaseConfig.java
@@ -5,9 +5,17 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/juevoJavaCRUD";
+    // private static final String URL = "jdbc:mysql://127.0.0.1:3306/juandavidgomezFiltro";
+    // private static final String USER = "root";
+    // private static final String PASSWORD = "juandavidgomez15@";
+
+    // public static Connection getConnection() throws SQLException {
+    //     return DriverManager.getConnection(URL, USER, PASSWORD);
+    // }
+
+    private static final String URL = "jdbc:mysql://localhost:3306/sgpzf";
     private static final String USER = "root";
-    private static final String PASSWORD = "campus2023";
+    private static final String PASSWORD = "123456";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
--- juegojava/src/main/java/com/javaplay/Main.java
+++ juegojava/src/main/java/com/javaplay/Main.java
@@ -1,5 +1,11 @@
 package com.javaplay;
 
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import java.awt.*;
 import person.application.personUseCase;
 import person.domain.service.personService;
 import person.infraestructure.inController.personaController;
@@ -7,9 +13,32 @@ import person.infraestructure.outRepository.personaRepository;
 
 public class Main {
     public static void main(String[] args) {
-        personService personaservice = new personaRepository();
-        personUseCase personausecase = new personUseCase(personaservice);
-        personaController personacontroller = new personaController(personausecase);
-        personacontroller.crearpersona();
+        JPanel panelpcincipal = new JPanel();
+        JButton crearpersona = new JButton("New Person");
+        JButton AsignarHabilidad = new JButton("asignar habiliad");
+        JButton crearHabilidad = new JButton("Creat skill");
+        panelpcincipal.setPreferredSize(new Dimension(400, 300));
+
+        panelpcincipal.setLayout(new GridLayout(2, 1));
+
+        panelpcincipal.add(crearpersona);
+        panelpcincipal.add(AsignarHabilidad);
+        panelpcincipal.add(crearHabilidad);
+        
+
+        crearpersona.addActionListener(e -> {
+            personService personaservice = new personaRepository();
+            personUseCase personausecase = new personUseCase(personaservice);
+            personaController personacontroller = new personaController(personausecase);
+            personacontroller.crearpersona();
+        });  
+
+        AsignarHabilidad.addActionListener(e->{
+            // Hexagonal de habilidad
+        });
+        crearHabilidad.addActionListener(e->{
+            // Hexagonal de crear Habilidad
+        });
+        
     }
 }
